9c8fba078cd0f237f890e9f459f1a5f92b246242
Daniel Lihotsky <dlihotsky@tudelft.nl>
2026-05-05 15:14:33 +0200

    log start and end and duration

--- App/params.h
+++ App/params.h
@@ -2,8 +2,8 @@
 #define PARAMS_H
 
 #define BASIC_ONEP
-#define MAX_NUM_NODES 5
-#define MAX_NUM_SIGNATURES 3
+#define MAX_NUM_NODES 11
+#define MAX_NUM_SIGNATURES 6
 #define MAX_NUM_TRANSACTIONS 400
 #define PAYLOAD_SIZE 0
 
